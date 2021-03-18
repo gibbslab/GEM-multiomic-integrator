557952d1f0da5359e2af37d077fa9174b33ea427
nmendozam <mendozamejian@gmail.com>
2021-03-18 15:20:53 -0500

    Support for TCDB added to get the protein codes in mapAbundanceToReactions

--- mapAbundanceToReactions.m
+++ mapAbundanceToReactions.m
@@ -4,7 +4,7 @@ function ProteinECNumbers = mapAbundanceToReactions(model, abundance)
 
 
 %% 1. Get the list of ProteinECNumbers
-ProteinECNumbers = getProteinECNumbers(abundance);
+ProteinECNumbers = getProteinCodes(abundance);
 
 
 
@@ -21,7 +21,7 @@ end
 
 
 
-function ProteinECNumbers = getProteinECNumbers (abundance)
+function ProteinECNumbers = getProteinCodes (abundance)
 import matlab.net.*
 import matlab.net.http.*
 
@@ -36,15 +36,18 @@ parfor i=1:IDs_length
     UniProtURL = ['https://www.uniprot.org/uniprot/' IDs{i} '.txt'];
     txt = webread(UniProtURL); 
     
-    UniProtData = regexp(txt, 'DE[ ]*EC=(?<ECNumbers>([0-9]+.([0-9]+.)*([0-9]|-)+))','names');
+    % Get the Enzyme Commission numbers
+    ECNumbers = regexp(txt, 'DE[ ]*EC=(?<ECNumbers>([0-9]+.([0-9]+.)*([0-9]|-)+))','names');
+    % Get the Transporter Classification numbers 
+    TCDBNumbers = regexp(txt, 'DR[ ]*TCDB; (?<TCDBNumbers>([0-9A-Z]+.([0-9A-Z]+.)*([0-9A-Z]|-)+))','names');
     
-    if not(isempty(UniProtData))
-       ProteinECNumbers{i} = reshape(struct2cell(UniProtData), numel(UniProtData), 1);
-    else
-       ProteinECNumbers{i} = {''};
-    end
+
+   ProteinECNumbers{i} = [
+       reshape(struct2cell(ECNumbers), numel(ECNumbers), 1)
+       reshape(struct2cell(TCDBNumbers), numel(TCDBNumbers), 1)
+       ];
     
-    waitbar(i/IDs_length, f,'Downloading ...');
+    waitbar(i/IDs_length, f,['Downloading (' num2str(i) '/' num2str(IDs_length) ')...']);
 end
 close(f)
 toc;
