7828e3525817269693d213a737a214aa16bb8214
nmendozam <mendozamejian@gmail.com>
2021-04-20 20:00:42 -0500

    Some additional steps and instructions

--- EC_modelo_Osorio.m
+++ EC_modelo_Osorio.m
@@ -1,7 +1,13 @@
+close all force; clear variables; clc
+%% Startup the COBRA Toolbox
+initCobraToolbox(false) % Don't update the toolbox
+changeCobraSolver('glpk', 'all');
+%% Load the model
 modelo=readCbModel('Astrocyte_Osorio2019.xml');
-reglas=modelo.rules;
-ECnumbers_osorio=readtable('ECnumbers_Osorio');
 
+% Go to https://www.uniprot.org/uploadlists/ select Entrez to UniprotKB
+% Enable the EC numbers column and download as excel
+ECnumbers_osorio=readtable('ECnumbers_Osorio.xlsx');
 Var_rem=table2array(ECnumbers_osorio(:,'ECNumber'));
 
 
