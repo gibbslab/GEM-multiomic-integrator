7a0c85cc20e0961ecc786f67276a36d3b31a2a23
nmendozam <mendozamejian@gmail.com>
2021-04-27 23:56:07 -0500

    Draft for creating Prules and ECNumbers list in Osorio's model

--- EC_modelo_Osorio.m
+++ EC_modelo_Osorio.m
@@ -3,20 +3,53 @@ close all force; clear variables; clc
 initCobraToolbox(false) % Don't update the toolbox
 changeCobraSolver('glpk', 'all');
 %% Load the model
-modelo=readCbModel('Astrocyte_Osorio2019.xml');
+model=readCbModel('Astrocyte_Osorio2019.xml');
 
 % Go to https://www.uniprot.org/uploadlists/ select Entrez to UniprotKB
 % Enable the EC numbers column and download as excel
 ECnumbers_osorio=readtable('ECnumbers_Osorio.xlsx');
-Var_rem=table2array(ECnumbers_osorio(:,'ECNumber'));
+ECNumbers=table2array(ECnumbers_osorio(:,'ECNumber'));
 
+ECNumbers = regexprep(ECNumbers,';$','');
+ECNumbers = strrep(ECNumbers,';',' & ');
+ECNumbers = strrep(ECNumbers,',',' & ');
+ECNumbers = strrep(ECNumbers,'or','|');
+ECNumbers = strrep(ECNumbers,'EC:','');
+ECNumbers = strrep(ECNumbers,'TCDB:','');
+ECNumbers = regexprep(ECNumbers, '\<(?!EC:|^\>)([0-9A-Z]+.(([0-9A-Z]|-)+.)*([0-9A-Z]|-)+)','x($1)');
 
-for i=1:2405
-    Secuencia=strcat(['x(',num2str(i),')'],'');
-    modelo.rules =  strrep(modelo.rules, Secuencia,Var_rem(i));
+unique_ECNumbers = {};
+sizeEcNumbers = 0;
+genes = regexp(model.rules,'(?<=x\()([0-9]+)(?=\))','match');
+model.Prules = model.rules;
+for i=1:numel(genes)
+    indexes = unique(str2double(genes{i}));
+    for j=1:numel(indexes)
+        index = indexes(j);
+        pattern = ['x(' num2str(index) ')'];
+        % We need to delete the entry and one of the logic characters around it
+        if isempty(ECNumbers{index})
+            regExp = regexptranslate('escape', pattern);
+            model.Prules(i) = regexprep(model.Prules(i), [regExp ' *[&|] *'], '');
+            model.Prules(i) = regexprep(model.Prules(i), ['( *[&|] *)?' regExp], '');
+        else
+            % Create the table of EC numbers
+            match = strcmp(ECNumbers{index}, unique_ECNumbers);
+        
+            if isempty(match) || sum(match) == 0 
+                sizeEcNumbers = sizeEcNumbers + 1;
+                ECNumberIndex = sizeEcNumbers;
+                % Save in the list of unique ECNumbers
+                unique_ECNumbers{sizeEcNumbers} = ECNumbers{index};
+            else
+                ECNumberIndex = find(match);
+            end
+            replacement = ['x(' num2str(ECNumberIndex) ')'];
+            model.Prules(i) = strrep(model.Prules(i), pattern, replacement);
+        end
+    end
 end
 
-
-%remp = '( )|';
-%modelo.rules =  strrep(modelo.rules, remp ,'');
-%modelo.rules =  strrep(modelo.rules, '\)','');
+model.Prules = regexprep(model.Prules,'((\||&) *\( *\) *)*$',''); % Remove emty parenthesis at the end
+model.Prules = regexprep(model.Prules,' *\( *\) *(\||&)?',''); % Remove emty parenthesis at the start and in the midle
+model.ECNumbers = unique_ECNumbers;
